6d7c4735130515d6c3ac7c9884b41639b27c0c41
Kenny_Lam <kenny.lam0802@gmail.com>
2023-11-21 12:54:30 -0700

    table rendering

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'Kobe', 'last_name' : 'Bryant'},
+        {'first_name' : 'LeBron', 'last_name' : 'James'},
+        {'first_name' : 'Nikola', 'last_name' : 'Jokic'}
+    ]
+    return render_template("index.html", users = users)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
